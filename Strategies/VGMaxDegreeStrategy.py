dce00acbf3668bf2616cd46aa88707e22928b94b
paj3r <ivo.pajer@gmail.com>
2024-06-23 15:01:45 +0200

    imprved long trading strategy.

--- Strategies/VGMaxDegreeStrategy.py
+++ Strategies/VGMaxDegreeStrategy.py
@@ -11,19 +11,32 @@ import networkx as nx
 from visibility_graph import generate_new_node_naive
 
 
-def VGCrossover_predictions(prices, positive):
-    out = np.zeros(len(prices))
-    if not positive:
-        prices = [-x for x in prices]
-    out[0] = 0
-    for i in range(1, len(prices)):
-        temp_prices = prices[:i]
-        graph = NaturalVG()
-        graph.build(temp_prices)
+def max_degree_values(values, lookback, positive):
+    avg_short_dist_p = np.zeros(len(values))
+    avg_short_dist_n = np.zeros(len(values))
 
-        matrix = graph.as_networkx()
-        out[i] = matrix.number_of_edges()/matrix.number_of_nodes()
-    return out
+    avg_short_dist_p[:] = np.nan
+    avg_short_dist_n[:] = np.nan
+
+    for i in range(lookback, len(values)):
+        dat = values[i - lookback + 1: i + 1]
+
+        pos = NaturalVG()
+        pos.build(dat)
+
+        neg = NaturalVG()
+        neg.build(-dat)
+
+        neg = neg.as_networkx()
+        pos = pos.as_networkx()
+
+        # you could replace shortest_path_length with other networkx metrics..
+        avg_short_dist_p[i] = max([pos.degree[x] for x in pos.nodes])
+        avg_short_dist_n[i] = max([neg.degree[x] for x in neg.nodes])
+
+        # Another possibility...
+
+    return avg_short_dist_p if positive else avg_short_dist_n
 
 
 def shortest_path_length(close: np.array, lookback: int, positive:bool):
@@ -53,17 +66,15 @@ def shortest_path_length(close: np.array, lookback: int, positive:bool):
 
     return avg_short_dist_p if positive else avg_short_dist_n
 
-class VGCrossoverLongStrategy(Strategy):
+class VGMaxDegreeStrategy(Strategy):
     # Define the two MA lengths, for the AMA' calculation.
     # Used for later optimisation of these values.
     ma_short_len = 10
     ma_long_len = 50
     lookback = 20
     def init(self):
-        self.vg_signals = self.I(VGCrossover_predictions, self.data.Close, True)
-        self.vg_signals_neg = self.I(VGCrossover_predictions, self.data.Close, False)
-        self.vg_spl_signals = self.I(shortest_path_length, self.data.Close, self.lookback, True)
-        self.vg_spl_signals_neg = self.I(shortest_path_length, self.data.Close, self.lookback,  False)
+        self.max_deg_vals = self.I(max_degree_values, self.data.Close, self.lookback, True)
+        self.max_deg_vals_neg = self.I(max_degree_values, self.data.Close, self.lookback, False)
         self.short_ma = self.I(talib.MA, self.data.Close, self.ma_short_len)
         self.long_ma = self.I(talib.MA, self.data.Close, self.ma_long_len)
         self.linreg = self.I(talib.LINEARREG, self.data.Close, self.ma_long_len)
@@ -83,14 +94,17 @@ class VGCrossoverLongStrategy(Strategy):
             # Bull regime.
             if self.position.is_short:
                 self.position.close()
-            if not self.position:
-                self.buy(sl=price - self.atr[-1] * 2)
-            # if not self.position and (crossover(self.vg_spl_signals, self.vg_spl_signals_neg) or crossover(self.vg_signals, self.vg_signals_neg)) and self.linreg[-1] > self.linreg[-2]:
+            # if not self.position:
             #     self.buy(sl = price - self.atr[-1]*2)
+            if (not self.position and self.max_deg_vals[-2] < self.max_deg_vals[-1]
+                    and self.max_deg_vals_neg[-2] > self.max_deg_vals_neg[-1]
+                    and self.linreg[-2] < self.linreg[-1]):
+                self.buy(sl=price - self.atr[-1]*2)
 
 
 dataframe = pd.read_csv("../OHLCTestData/btcusd_ohlc.csv", index_col=0, parse_dates=True, infer_datetime_format=True)
-bt = Backtest(dataframe, VGCrossoverLongStrategy, cash=100000, exclusive_orders=True)
+bt = Backtest(dataframe, VGMaxDegreeStrategy, cash=100000, exclusive_orders=True)
+# stats = bt.optimize(lookback=range(10, 50, 5), maximize='Return [%]')
 # stats = bt.optimize(behind=range(2, 50, 1), maximize='Return [%]')
 # stats = bt.optimize(
 #     short_length=range(5, 20, 5),
